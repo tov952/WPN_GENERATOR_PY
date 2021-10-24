01e9df8372306e1803cb732139ab9eebcaca1ea7
Jason <rotaprita@gmail.com>
2021-10-24 19:18:24 +0800

    created GRP.py for upgrade

--- WPN_Generator_GRP.py
+++ WPN_Generator_GRP.py
@@ -1,26 +1,32 @@
 import sys
 import hou
 import pprint
+import os
 import itertools
 from WPN_GENERATOR_PY import WPN_Enums
 from WPN_GENERATOR_PY import WPN_Utils
 sys.path.append(r"E:\Users\Jason\Documents\houdini18.5\python3.7libs\WPN_GENERATOR_PY\venv\Lib\site-packages")
 
-from WPN_GENERATOR_PY import GunPart
+from WPN_GENERATOR_PY import GunPart_GRP as GunPart
 from WPN_GENERATOR_PY import gunPartParmTemplatesGRP as GPpT
 
 import fnmatch
 from psd_tools import PSDImage
+from psd_tools.constants import Resource
 
+
+#GLOBALS
 GEO_CONTAINER = "GEO_CONTAINER"
-
 debug = True
 
 gunPartList = []   #Stores instances of generated gunparts
 parentChildDict = {} #Stores dict of <Group> : <Layer>/<Group> Children[]
+
+psdFilepath = ""
 psd = None #stores PSD
 savedPSD = None #Stores the saved PSD
 groupParentDict = {} #Stores dict of <Group> : str parentName
+
 
 
 def rebuild(this_node):
@@ -34,62 +40,56 @@ def rebuild(this_node):
     # Cleans Geo Container
     for child in geoContainer.children():
         child.destroy()
-    print("---------------------Get Parent-Child Dict----------------------------")
+    print("---------------------Get Parent-Child Dict---------------------------")
     PCDict = getPSDGrpAndChildren(this_node)
-
-    #print(PCDict)
+    print("---------------------Saving Renamed PSDs-----------------------------")
     savedPSDPath = renameChildLayersAndSave(PCDict)
     savedPSD = PSDImage.open(savedPSDPath)
-
-    #ptg = this_node.parmTemplateGroup()
-    #folder = hou.FolderParmTemplate("folder", "My Parms")
-
+    print("---------------------Get Group:Parent Dict---------------------------")
     groupParentDict = getGroupParentDict()
-    pprint.pprint(groupParentDict)
-    #print(folder)
-    #ptg.append(folder)
-    parmName = []
-    pprint.pprint(parentChildDict)
-    for parent, children in parentChildDict.items():
-        containingFolder = ()
-        parmNames = [ parm.name() for parm in this_node.parms()]
-        #print(parent.name)
-        #print(parmNames)
-        if parent.name not in parmNames:
-            parentParmTemplate = buildParmTemplates(this_node, parent.name, Group = True)
-        else:
-            i = parmNames.index(parent.name)
-            parentParmTemplate = this_node.parms()[i].parmTemplate()
-        #print(parentParmTemplate)
-        #ptg.append(parentParmTemplate)
-        for child in children:
-            if child.is_group() != True:
-                splitChildName = child.name.split("_")
-                if "SIDE" in splitChildName or "CUTOUT" in splitChildName:
-                    parentParmTemplate.addParmTemplate(buildParmTemplates(this_node, child.name))
-        if parentParmTemplate != None:
-            if parent.is_group:
-                containingFolder = groupParentDict[parent]
-                print(containingFolder)
-            this_node.addSpareParmTuple(parentParmTemplate, in_folder = containingFolder)
-    #print(ptg.parmTemplates())
 
-
-
-    print("---------------------Getting Valid PSD Layer Names-------------------")
-    #validPSDLayerNames, layerNameCountDict = getValidPSDLayerNames(this_node)
     print("---------------------Building Parm Templates-------------------------")
-    #buildParmTemplates(this_node, validPSDLayerNames)
-    print("---------------------Setting MultiParmNums---------------------------")
-    #SetMultiParmNums(layerNameCountDict, this_node)
+    BuildParmTemplateHeirarchy(groupParentDict, this_node)
     print("---------------------Generating Gun Parts----------------------------")
-    #genGunPartNodes(geoContainer, this_node)
+    genGunPartNodes(geoContainer, this_node)
     print("---------------------Setting Layer Names-----------------------------")
     #SetValidLayerName(this_node, validPSDLayerNames)
     print("---------------------Setting Gun Part Shape--------------------------")
     #for gunpart in gunPartList:
         #gunpart.setValidLayerName()
         #gunpart.setShapeType()
+
+
+def BuildParmTemplateHeirarchy(groupParentDict, this_node):
+    #Build Parent Parm Templates
+    for parent, children in parentChildDict.items():
+        containingFolder = ()
+        parmNames = [parm.name() for parm in this_node.parms()]
+        if parent.name not in parmNames:
+            parentParmTemplate = buildParmTemplates(this_node, parent.name, Group=True)
+        else:
+            i = parmNames.index(parent.name)
+            parentParmTemplate = this_node.parms()[i].parmTemplate()
+
+        #Build Child Templates
+        for child in children:
+            if child.is_group() != True:
+                splitChildName = child.name.split("_")
+                if "SIDE" in splitChildName or "CUTOUT" in splitChildName:
+                    parentParmTemplate.addParmTemplate(buildParmTemplates(this_node, child.name))
+                print("Parenting " + child.name + " Parms into " + parent.name + " ParmFolder")
+        if parentParmTemplate != None:
+            if parent.is_group:
+                containingFolder = groupParentDict[parent]
+                if len(containingFolder) != 0:
+                    parentFolder = containingFolder[-1] + " ParmFolder"
+                else:
+                    parentFolder = "Root"
+                print("Parenting " + parent.name + " ParmFolder into " + parentFolder)
+
+            this_node.addSpareParmTuple(parentParmTemplate, in_folder=containingFolder)
+
+
 
 def removeNone(list):
     strList = []
@@ -120,11 +120,16 @@ def getGroupParentDict():
         flatParentList = flatten(cleanParentlist)
         flatParentList.reverse()
         pop = flatParentList.pop(0)
-        print(flatParentList)
         groupParentDict[parent] = tuple(flatParentList)
-
+        if len(flatParentList) != 0:
+            printParentList = ", ".join(flatParentList) + " ParmFolders" #Only for printing
+        else:
+            printParentList = "None" #Only for printing
+        print(parent.name + " ParmFolder's Ancestors are: " + printParentList)
+    if debug:
+        print("DEBUG: Group Parent Dict  ( <Group> : Tuple of all Parents.name ) ")
+        pprint.pprint(groupParentDict)
     return groupParentDict
-    #print(groupParentDict)
 
 def getParentOfLayer(layer):
     #print(layer.name)
@@ -144,35 +149,48 @@ def SetValidLayerName(this_node, validPSDLayerNames):
 
 
 def renameChildLayersAndSave(PCDict):
-    psdPath = "E:/psdTest.psd"
+    (dirname, filename) = os.path.split(psdFilepath)
+    (filename, ext) = os.path.splitext(filename)
+    psdPath = dirname + "/" + filename + "_renamed" + ext
     for parent, children in PCDict.items():
         for child in children:
             ogChildName = child.name
             newChildName = parent.name + "_" + child.name
             child.name = newChildName
-    #print(psd)
+            if debug:
+                print("DEBUG: Renaming " + ogChildName + " to " + newChildName)
     psd.save(psdPath)
+    print("Saved Renamed PSD into: " + psdPath)
     return psdPath
 
 
 
 def genGunPartNodes(geoContainer, this_node):
     # Gen Gunpart Nodes as needed
-    gpTypes = WPN_Enums.GPTypes
-    for gpType in gpTypes:
-        multiparm = this_node.parm("numOf_" + gpType.name)
-        if multiparm != None:
-            #print(gpType.name + " multiparm found :" + multiparm.name())
-            #print("MultiParm Length :" + str(multiparm.eval()))
-            multiparmLen = multiparm.eval()
-            for i in range(multiparmLen):
-                gunPartNodeName = gpType.name
-                gunPart = GunPart.gunpart(gunPartNodeName, gpType, i)
-                gunPartNode = gunPart.createGunpartNode()
-                gunPartList.append(gunPart)
-                #print(gunPartNode)
-                gunPartNode.parm("file").set(WPN_Utils.linkExpressionSTR("file", True, True))
-                linkParms(this_node, gunPartNode, gunPart.parmPrefix)
+
+    for group in groupParentDict.keys():
+        gunPartNodeName = group.name
+        gunPart = GunPart.gunpart(gunPartNodeName)
+        gunPartNode = gunPart.createGunpartNode()
+        gunPartList.append(gunPart)
+        #print(gunPartNode)
+        gunPartNode.parm("file").set(WPN_Utils.linkExpressionSTR("file", True, True))
+        linkParms(this_node, gunPartNode, gunPart.parmPrefix)
+
+    # for gpType in gpTypes:
+    #     multiparm = this_node.parm("numOf_" + gpType.name)
+    #     if multiparm != None:
+    #         #print(gpType.name + " multiparm found :" + multiparm.name())
+    #         #print("MultiParm Length :" + str(multiparm.eval()))
+    #         multiparmLen = multiparm.eval()
+    #         for i in range(multiparmLen):
+    #             gunPartNodeName = gpType.name
+    #             gunPart = GunPart.gunpart(gunPartNodeName, gpType, i)
+    #             gunPartNode = gunPart.createGunpartNode()
+    #             gunPartList.append(gunPart)
+    #             #print(gunPartNode)
+    #             gunPartNode.parm("file").set(WPN_Utils.linkExpressionSTR("file", True, True))
+    #             linkParms(this_node, gunPartNode, gunPart.parmPrefix)
 
     geoContainer.layoutChildren()
     for child in geoContainer.children():
@@ -205,27 +223,23 @@ def buildParmTemplates(this_node, layerName, Group = False):
 def getPSDGrpAndChildren(this_node):
     # Gets list of Grps
     global psd
-    filepath = this_node.parm("file").eval()
-    psd = PSDImage.open(filepath)
-    #print(psd)
-    layerNames = []
-    validLayerNames = []
-    # print(gpTypesNames)
-
+    global psdFilepath
+    psdFilepath = this_node.parm("file").eval()
+    psd = PSDImage.open(psdFilepath)
     for i, layer in enumerate(psd):
         getChildOfLayer(layer)
-    #print(parentChildDict)
-
-
+    if debug:
+        print("DEBUG: ParentChildDict ( Parent <Group> : Child <Group>/<Layer> )")
+        pprint.pprint(parentChildDict)
 
 
     return parentChildDict
-    #print(parentChildDict)
 
 
 def getChildOfLayer(layer):
     if layer.is_group():
-        print(layer.name + "is Group")
+        if debug:
+            print( "DEBUG: " + layer.name + " is Group")
         for childLayer in layer:
             parentName = layer.name
             if debug == True:
